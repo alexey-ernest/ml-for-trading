8ba6b3eccfd86721f3f89dc4a72466b297a08e37
alexey-ernest <alexey.ernest@gmail.com>
2016-11-21 11:45:22 +0300

    Two columns plotting

--- plot.py
+++ plot.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 def test_run():
   df = pd.read_csv("data/AAPL.csv")
-  print df["Adj Close"]
-  df["Adj Close"].plot()
+  
+  print df[["Close", "Adj Close"]]
+  df[["Close", "Adj Close"]].plot()
   plt.show() # must be called to show plots
 
 if __name__ == "__main__":
